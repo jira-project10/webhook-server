f37d3a3eae0019629473eb01028ecbbcfad09581
jira-project10 <129411113+jira-project10@users.noreply.github.com>
2023-03-30 20:48:48 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(name)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('https://example.com/rest/webhooks/webhook1', methods=['POST'])
 def handle_webhook():
     data = request.json
     issue_id = data['issue']['id']
@@ -10,9 +10,9 @@ def handle_webhook():
     issue_description = data['issue']['fields']['description']
 
     # Process the new ticket here, e.g. send a notification to Slack
-    print(f"New ticket created: ID {issue_id}, Title: {issue_title}, Description: {issue_description}")
-
-    return '', 200
+    #print(f"New ticket created: ID {issue_id}, Title: {issue_title}, Description: {issue_description}")
+    #return '', 200
+    return render_template('ticket.html', issue_id=issue_id, issue_title=issue_title, issue_description=issue_description)
 
 if name == 'main':
     app.run(debug=True, port=5000)
