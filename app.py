9fee737a65f9886bd545420b3b4318ad401ac145
jira-project10 <129411113+jira-project10@users.noreply.github.com>
2023-04-01 11:02:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,22 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-@app.route('/', methods=['POST'])
+@app.route('/webhook1', methods=['POST'])
 def handle_webhook():
     data = request.json
     issue_id = data['issue']['id']
     issue_title = data['issue']['fields']['summary']
     issue_description = data['issue']['fields']['description']
 
+    app.logger.info('%s logged in successfully', issue_id)
+    app.logger.info('%s logged in successfully',issue_title)
+    app.logger.info('%s logged in successfully', issue_description)
+
     # Process the new ticket here, e.g. send a notification to Slack
-    print(f"New ticket created: ID {issue_id}, Title: {issue_title}, Description: {issue_description}")
+    #print(f"New ticket created: ID {issue_id}, Title: {issue_title}, Description: {issue_description}")
     #return '', 200
-    return 'ticket created'
+    #return 'ticket created'
     #return render_template('ticket.html', issue_id=issue_id, issue_title=issue_title, issue_description=issue_description)
 
 
 app.run(debug=True, port=os.environ.get('PORT', 5000))
-
